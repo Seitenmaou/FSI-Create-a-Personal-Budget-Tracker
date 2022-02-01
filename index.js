658ec58d4acbd513a738ea54b5fe9f8237394e99
Euri Tanaka <97496667+Seitenmaou@users.noreply.github.com>
2022-01-31 19:38:16 -0800

    Add comments to index.js

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ let useWhileLoop = false;
 //default false, reverses question order aka iteraion when true
 let reverseQuestionOrder =false;
 
+//use while loop
 if (useWhileLoop)
 {
+    //use while loop and ask question in reverse order
     if (reverseQuestionOrder)
     {
+        //ask questions on weekly expences, while loop, reverse
         let i = weeklyExpenseQuestions.length - 1;
         while (i >= 0)
         {
@@ -20,6 +23,7 @@ if (useWhileLoop)
             weeklyExpenses += userAnswer
             i--
         }
+        //ask questions on monthly expences, while loop, reverse
         i = monthlyExpenseQuestions.length - 1;
         while (i >= 0)
         {
@@ -27,6 +31,7 @@ if (useWhileLoop)
             monthlyExpenses += userAnswer
             i--
         }
+        //ask questions on annual expences, while loop, reverse
         i = annualExpenseQuestions.length - 1;
         while (i >= 0)
         {
@@ -35,8 +40,10 @@ if (useWhileLoop)
             i--
         }
     }
+    //use while loop and ask question in chronological order
     else
     {
+        //ask questions on weekly expences, while loop, chronological
         let i = 0;
         while (i < weeklyExpenseQuestions.length)
         {
@@ -44,6 +51,7 @@ if (useWhileLoop)
             weeklyExpenses += userAnswer
             i++
         }
+        //ask questions on monthly expences, while loop, chronological
         i = 0;
         while (i < monthlyExpenseQuestions.length)
         {
@@ -51,6 +59,7 @@ if (useWhileLoop)
             monthlyExpenses += userAnswer
             i++
         }
+        //ask questions on annual expences, while loop, chronological
         i = 0;
         while (i < annualExpenseQuestions.length)
         {
@@ -60,42 +69,47 @@ if (useWhileLoop)
         }
     }
 }
+//use for loop
 else
 {
+    //use for loop and ask question in reverse order
     if (reverseQuestionOrder)
     {
+        //ask questions on weekly expences, for loop, reverse
         for(let i = weeklyExpenseQuestions.length - 1; i >= 0; i--)
         {
         let userAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
         weeklyExpenses += userAnswer
         }
-
+        //ask questions on monthly expences, for loop, reverse
         for(let i = monthlyExpenseQuestions.length - 1; i >= 0; i--)
         {
         let userAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
         monthlyExpenses += userAnswer
         }
-
+        //ask questions on annual expences, for loop, reverse
         for(let i = annualExpenseQuestions.length - 1; i >= 0; i--)
         {
         let userAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
         annualExpenses += userAnswer
         }
     }
+    //use for loop and ask question in chronological order
     else
     {
+        //ask questions on weekly expences, for loop, chronological
         for(let i = 0; i < weeklyExpenseQuestions.length; i++)
         {
         let userAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
         weeklyExpenses += userAnswer
         }
-
+        //ask questions on monthly expences, for loop, chronological
         for(let i = 0; i < monthlyExpenseQuestions.length; i++)
         {
         let userAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
         monthlyExpenses += userAnswer
         }
-
+        //ask questions on annual expences, for loop, chronological
         for(let i = 0; i < annualExpenseQuestions.length; i++)
         {
         let userAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
